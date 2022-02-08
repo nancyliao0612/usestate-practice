dbfbd8415db8911388f8f2f32b601a9cbb38fd20
Nancy <nancy62924@gmail.com>
2022-02-08 20:54:32 +0800

    fill or empty the star

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Star from "./Star";
 
 function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: true,
+  });
+
+  function toggleFavorite() {
+    setContact((prevState) => {
+      return { ...prevState, isFavorite: !prevState.isFavorite };
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article className="card">
+        <img src={require("./images/user.png")} className="card--image" />
+        <div className="card--info">
+          <Star isFilled={contact.isFavorite} handleClick={toggleFavorite} />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import Count from "./Count";
+
+// export default function App() {
+//   const [count, setCount] = React.useState(0);
+
+//   function add() {
+//     setCount((prevCount) => prevCount + 1);
+//   }
+
+//   function subtract() {
+//     setCount((prevCount) => prevCount - 1);
+//   }
+//   /**
+//    * Challenge:
+//    * - Create a new component named Count
+//    *    - It should receive a prop called `number`, whose value
+//    *      is the current value of our count
+//    *    - Have the component render the whole div.counter--count
+//    *      and display the incoming prop `number`
+//    * - Replace the div.counter--count below with an instance of
+//    *   the new Count component
+//    */
+//   return (
+//     <div className="counter">
+//       <button className="counter--minus" onClick={subtract}>
+//         –
+//       </button>
+//       <Count number={count} />
+//       <button className="counter--plus" onClick={add}>
+//         +
+//       </button>
+//     </div>
+//   );
+// }
---
+++ src/Star.js
@@ -0,0 +1,11 @@
+export default function Star(props) {
+  let starIcon = props.isFilled ? "star-filled.png" : "star-empty.png";
+
+  return (
+    <img
+      src={require(`./images/${starIcon}`)}
+      className="card--favorite"
+      onClick={props.handleClick}
+    />
+  );
+}
